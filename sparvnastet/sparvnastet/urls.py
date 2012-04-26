c5b46bbbebf517451393dca369933dd18f22a82b
Felix Panozzo <fpanozzo@gmail.com>
2012-04-26 17:08:00 +0200

    added url to 404 page, to test it

--- sparvnastet/sparvnastet/urls.py
+++ sparvnastet/sparvnastet/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.generic.simple import direct_to_template
 from sparvnastet.apps.pages.views import PageView
 
 DIRNAME = os.path.dirname(__file__)
@@ -16,6 +17,7 @@ urlpatterns = patterns('',
     (r'^media/(?P<path>.*)$', 'django.views.static.serve',
      {'document_root': os.path.join(DIRNAME, "media"), 'show_indexes': True }),
     url(r'^p/(?P<slug>[-\w]*)/$', PageView.as_view(), name="page_view"),
+    ('^404/$', direct_to_template, {'template': '404.html'})
 )
 
 urlpatterns += staticfiles_urlpatterns()
